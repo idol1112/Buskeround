5fc7fa3e3a50342a316ee0461dd969b8d93c8bab
Maroban <dbtmdqja207@naver.com>
2021-09-24 18:07:43 +0900

    api 수정 및 PostVo 추가(2021.09.24 18:07) - 유승범

--- src/main/java/com/javaex/api/controller/ApiBlogController.java
+++ src/main/java/com/javaex/api/controller/ApiBlogController.java
@@ -1,37 +1,12 @@
 package com.javaex.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.javaex.service.UserService;
 
-@RequestMapping(value = "/api/user/")
+@RequestMapping(value = "/api/blog/")
 @Controller
 public class ApiBlogController {
 
-  @Autowired
-  UserService userService;
-
-  /*** 아이디 중복체크 ***/
-  @ResponseBody
-  @RequestMapping(value = "idCheck", method = {RequestMethod.GET, RequestMethod.POST})
-  public boolean idCheck(@RequestParam("id") String id) {
-    System.out.println("[현재 위치: ApiUserController.idCheck]");
-
-    return userService.idCheck(id);
-  }
-
-  /*** 닉네임 중복체크 ***/
-  @ResponseBody
-  @RequestMapping(value = "nickCheck", method = {RequestMethod.GET, RequestMethod.POST})
-  public boolean nickCheck(@RequestParam("nickname") String nickname) {
-    System.out.println("[현재 위치: ApiUserController.idCheck]");
-
-    return userService.nickCheck(nickname);
-  }
 
 }
 
--- src/main/java/com/javaex/vo/PostVo.java
+++ src/main/java/com/javaex/vo/PostVo.java
@@ -2,15 +2,166 @@ package com.javaex.vo;
 
 public class PostVo {
 
-  //필드
+  // 필드
+  private int user_no;
+  private int live;
+  private int post_no;
+  private int category;
+  private String title;
+  private String content;
+  private String reg_date;
+  private int latitude;
+  private int longitude;
+  private String p_start;
+  private String p_end;
+  private String p_img;
+  private String live_url;
+  private int hit;
 
+  // 생성자
+  public PostVo() {
+    super();
+  }
 
-  //생성자
+  public PostVo(int user_no, int live, int post_no, int category, String title, String content, String reg_date, int latitude, int longitude,
+      String p_start, String p_end, String p_img, String live_url, int hit) {
+    super();
+    this.user_no = user_no;
+    this.live = live;
+    this.post_no = post_no;
+    this.category = category;
+    this.title = title;
+    this.content = content;
+    this.reg_date = reg_date;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.p_start = p_start;
+    this.p_end = p_end;
+    this.p_img = p_img;
+    this.live_url = live_url;
+    this.hit = hit;
+  }
 
+  // 메소드 - GS
+  public int getUser_no() {
+    return user_no;
+  }
 
-  //메소드 - GS
+  public void setUser_no(int user_no) {
+    this.user_no = user_no;
+  }
 
+  public int getLive() {
+    return live;
+  }
 
-  //메소드 - 일반
+  public void setLive(int live) {
+    this.live = live;
+  }
+
+  public int getPost_no() {
+    return post_no;
+  }
+
+  public void setPost_no(int post_no) {
+    this.post_no = post_no;
+  }
+
+  public int getCategory() {
+    return category;
+  }
+
+  public void setCategory(int category) {
+    this.category = category;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getReg_date() {
+    return reg_date;
+  }
+
+  public void setReg_date(String reg_date) {
+    this.reg_date = reg_date;
+  }
+
+  public int getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(int latitude) {
+    this.latitude = latitude;
+  }
+
+  public int getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(int longitude) {
+    this.longitude = longitude;
+  }
+
+  public String getP_start() {
+    return p_start;
+  }
+
+  public void setP_start(String p_start) {
+    this.p_start = p_start;
+  }
+
+  public String getP_end() {
+    return p_end;
+  }
+
+  public void setP_end(String p_end) {
+    this.p_end = p_end;
+  }
+
+  public String getP_img() {
+    return p_img;
+  }
+
+  public void setP_img(String p_img) {
+    this.p_img = p_img;
+  }
+
+  public String getLive_url() {
+    return live_url;
+  }
+
+  public void setLive_url(String live_url) {
+    this.live_url = live_url;
+  }
+
+  public int getHit() {
+    return hit;
+  }
+
+  public void setHit(int hit) {
+    this.hit = hit;
+  }
+
+  // 메소드 - 일반
+  @Override
+  public String toString() {
+    return "PostVo [user_no=" + user_no + ", live=" + live + ", post_no=" + post_no + ", category=" + category + ", title=" + title + ", content="
+        + content + ", reg_date=" + reg_date + ", latitude=" + latitude + ", longitude=" + longitude + ", p_start=" + p_start + ", p_end=" + p_end
+        + ", p_img=" + p_img + ", live_url=" + live_url + ", hit=" + hit + "]";
+  }
+
 
 }
