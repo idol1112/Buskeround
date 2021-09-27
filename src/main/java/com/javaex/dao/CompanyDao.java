b3a650b0e4635375e581f0f68935a737aff40c86
hi <hi@DESKTOP-OAV2V8U>
2021-09-27 17:36:01 +0900

    공연장 리스트/수정/삭제ajax(2021-09-27 17:35)-김윤형

--- src/main/java/com/javaex/dao/CompanyDao.java
+++ src/main/java/com/javaex/dao/CompanyDao.java
@@ -1,10 +1,13 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.CompanyVo;
+import com.javaex.vo.StageVo;
 
 @Repository
 public class CompanyDao {
@@ -12,6 +15,7 @@ public class CompanyDao {
 	@Autowired
 	SqlSession sqlSession;
 	
+	///////////////////////////////// 제휴사 /////////////////////////////////
 	//제휴사 등록
 	public int companyInsert(CompanyVo companyVo) {
 		System.out.println("CompanyDao.companyInsert()");
@@ -32,4 +36,43 @@ public class CompanyDao {
 		
 		return sqlSession.update("company.companyUpdate", companyVo);
 	}
+	///////////////////////////////// *제휴사* /////////////////////////////////
+	
+///////////////////////////////// 공연장 /////////////////////////////////
+	//공연장 리스트 불러오기
+	public List<StageVo> selectStage(int user_no) {
+		System.out.println("CompanyDao.selectStage()");
+		
+		return sqlSession.selectList("stage.selectStage", user_no);
+	}
+	
+	//공연장 리스트 하나 불러오기
+	public StageVo selectStageOne(int stage_no) {
+		System.out.println("CompanyDao.selectStageOne()");
+		
+		return sqlSession.selectOne("stage.selectStageOne", stage_no);
+	}
+	
+	//공연장 등록
+	public int stageInsert(StageVo stageVo) {
+		System.out.println("CompanyDao.stageInsert()");
+		
+		return sqlSession.insert("stage.stageInsert", stageVo);
+	}
+	
+	//공연장 수정
+	public int stageUpdate(StageVo stageVo) {
+		System.out.println("CompanyDao.stageUpdate()");
+		
+		return sqlSession.update("stage.stageUpdate", stageVo);
+	}
+	
+	//공연장 삭제
+	public int stageDelete(int stage_no) {
+		System.out.println("CompanyDao.stageDelete()");
+		
+		return sqlSession.delete("stage.stageDelete", stage_no);
+	}
+
+///////////////////////////////// *공연장* /////////////////////////////////
 }
