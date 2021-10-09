596976223ca3867ba4045113f10ed54e66c33a7e
Maroban <dbtmdqja207@naver.com>
2021-10-09 21:59:53 +0900

    충돌 난 것 수정(2021.10.09 22:00) - 유승범

--- src/main/java/com/javaex/controller/BlogController.java
+++ src/main/java/com/javaex/controller/BlogController.java
@@ -1,8 +1,9 @@
 package com.javaex.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.javaex.service.ArtistService;
 import com.javaex.service.BlogService;
 import com.javaex.service.BlogService2;
@@ -24,186 +24,198 @@ import com.javaex.vo.ResumeVo;
 @Controller
 public class BlogController {
 
-	@Autowired
-	BlogService blogService;
+  @Autowired
+  BlogService blogService;
 
-    @Autowired
-    ArtistService artistService;
+  @Autowired
+  ArtistService artistService;
 
-    @Autowired
-    BlogService2 blogService2;
+  @Autowired
+  BlogService2 blogService2;
 
-	@RequestMapping(value = "blog_main/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_main(@PathVariable("id") String id, Model model) {
-		System.out.println("[BlogController.blog_main()]");
+  @RequestMapping(value = "blog_main/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_main(@PathVariable("id") String id, Model model) {
+    System.out.println("[BlogController.blog_main()]");
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute(blogVo);
 
-		// 이력사항 가져오기
-		List<ResumeVo> resumeList = blogService.getResumeList(id);
-		model.addAttribute("resumeList", resumeList);
+    // 이력사항 가져오기
+    List<ResumeVo> resumeList = blogService.getResumeList(id);
+    model.addAttribute("resumeList", resumeList);
 
-		//Aside 리스트
-		model.addAttribute("BlogLive", artistService.getBlogLive());
-		System.out.println("아티스트 라이브 리스트: " + artistService.getBlogLive());
+    // Aside 리스트
+    model.addAttribute("BlogLive", artistService.getBlogLive());
+    System.out.println("아티스트 라이브 리스트: " + artistService.getBlogLive());
 
-		// 갤러리 리스트
-		 model.addAttribute("galleryList", blogService2.getGalleryMain(blogVo.getUser_no()));
+    // 갤러리 리스트
+    model.addAttribute("galleryList", blogService2.getGalleryMain(blogVo.getUser_no()));
 
-		 System.out.println(blogService2.getGalleryMain(blogVo.getUser_no()));
+    System.out.println("BlogVo: " + blogVo);
 
-		 System.out.println("BlogVo: " + blogVo);
+    return "Blog/blog_main";
 
-		 return "Blog/blog_main";
+  }
 
-	}
+  // ModifyForm
+  @RequestMapping(value = "blog_modify/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_modifyform(@PathVariable("id") String id, Model model) {
+    System.out.println("[BlogController.blog_modify()]");
 
-	//ModifyForm
-	@RequestMapping(value = "blog_modify/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_modifyform(@PathVariable("id") String id, Model model) {
-		System.out.println("[BlogController.blog_modify()]");
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute("blogVo", blogVo);
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute("blogVo", blogVo);
+    // 이력사항 가져오기
+    List<ResumeVo> resumeList = blogService.getResumeList(id);
+    model.addAttribute("resumeList", resumeList);
 
-		// 이력사항 가져오기
-		List<ResumeVo> resumeList = blogService.getResumeList(id);
-		model.addAttribute("resumeList", resumeList);
+    return "Blog/blogModifyForm";
+  }
 
-		return "Blog/blogModifyForm";
-	}
+  // 수정
+  @RequestMapping(value = "modify", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_modify(@ModelAttribute BlogVo blogVo,
+      @RequestParam(value = "resume_content[]", required = false, defaultValue = "0") List<String> resumeContentList,
+      @RequestParam(value = "file1", required = false, defaultValue = "0") MultipartFile file, @RequestParam("img_check") int img) {
+    System.out.println("[BlogController.modify()]");
 
-	// 수정
-	@RequestMapping(value = "modify", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_modify(@ModelAttribute BlogVo blogVo,
-			@RequestParam(value = "resume_content[]", required = false, defaultValue="0") List<String> resumeContentList,
-			@RequestParam(value = "file1", required = false, defaultValue = "0") MultipartFile file,
-			@RequestParam("img_check") int img) {
-		System.out.println("[BlogController.modify()]");
+    // 확인
+    System.out.println("modify.blogVo: " + blogVo);
+    System.out.println("modify.file: " + file);
+    System.out.println("imgCheck: " + img);
+    System.out.println("이력사항");
+    // 이력사항
+    for (String s : resumeContentList) {
+      System.out.println(s);
+    }
 
-		// 확인
-		System.out.println("modify.blogVo: " + blogVo);
-		System.out.println("modify.file: " + file);
-		System.out.println("imgCheck: " + img);
-		System.out.println("이력사항");
-		//이력사항
-		for(String s : resumeContentList) {
-			System.out.println(s);
-		}
+    List<ResumeVo> resumeList = new ArrayList<>();
+    for (String s : resumeContentList) {
 
-		List<ResumeVo> resumeList = new ArrayList<>();
-		for(String s : resumeContentList) {
+      resumeList.add(new ResumeVo(0, blogVo.getUser_no(), s));
+    }
+    System.out.println("리스트 출력" + resumeList);
 
-			resumeList.add(new ResumeVo(0, blogVo.getUser_no(), s));
-		}
-		System.out.println("리스트 출력" + resumeList);
+    if (img == 2) {
+      // 이미지 삭제
+      int count = blogService.modifyDeleteImg(blogVo, resumeList);
 
-		if (img == 2) {
-			// 이미지 삭제
-			int count = blogService.modifyDeleteImg(blogVo, resumeList);
+      // 수정
+      System.out.println("수정 완료 여부: " + count);
+    } else {
+      // 이미지 X
+      if (file.isEmpty()) {
+        int count = blogService.modify(blogVo, resumeList);
+        // 수정
+        System.out.println("수정 완료 여부: " + count);
 
-			// 수정
-			System.out.println("수정 완료 여부: " + count);
-		} else {
-			// 이미지 X
-			if (file.isEmpty()) {
-				int count = blogService.modify(blogVo, resumeList);
-				// 수정
-				System.out.println("수정 완료 여부: " + count);
+      } else {
+        // 이미지 수정
+        int count = blogService.modifyImg(blogVo, file, resumeList);
 
-			} else {
-				// 이미지 수정
-				int count = blogService.modifyImg(blogVo, file, resumeList);
+        // 수정
+        System.out.println("수정 완료 여부: " + count);
+      }
 
-				// 수정
-				System.out.println("수정 완료 여부: " + count);
-			}
+    }
 
-		}
-
-		// 수정 완료
-		return "redirect:/blog/blog_main/" + blogVo.getId();
-	}
+    // 수정 완료
+    return "redirect:/blog/blog_main/" + blogVo.getId();
+  }
 
 
-	@RequestMapping(value = "blog_timeline/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_timeline(@PathVariable("id") String id, Model model) {
-		System.out.println("[TestingController.blog_timeline()]");
+  @RequestMapping(value = "blog_timeline/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_timeline(@PathVariable("id") String id, Model model) {
+    System.out.println("[TestingController.blog_timeline()]");
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute(blogVo);
 
-		return "Blog/blog_timeline";
+    return "Blog/blog_timeline";
+  }
 
-	}
+  @RequestMapping(value = "blog_guestbook/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_guestbook(@PathVariable("id") String id, Model model) {
+    System.out.println("[TestingController.blog_guestbook()]");
 
-	@RequestMapping(value = "blog_guestbook/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_guestbook(@PathVariable("id") String id, Model model) {
-		System.out.println("[TestingController.blog_guestbook()]");
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute(blogVo);
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    return "Blog/blogGuestbookBoard";
+  }
 
-		return "Blog/blogGuestbookBoard";
+  @RequestMapping(value = "blog_notice/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_noticeboard(@PathVariable("id") String id, @RequestParam(value = "keyword", required = false) String keyword, Model model) {
+    System.out.println("[TestingController.blog_notice()]");
 
-	}
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute(blogVo);
 
-	@RequestMapping(value = "blog_notice/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_noticeboard(@PathVariable("id") String id, Model model) {
-		System.out.println("[TestingController.blog_notice()]");
+    // 서치 값 맵에 담기
+    Map<String, Object> searchvalue = new HashMap<String, Object>();
+    searchvalue.put("search", keyword);
+    searchvalue.put("id", id);
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    // 리스트 가져오기
+    List<NoticeVo> noticeList = blogService.noticeList(searchvalue);
+    System.out.println(noticeList);
+    model.addAttribute("noticeList", noticeList);
 
-		// 리스트 가져오기
-//		List<NoticeVo> list = blogService.noticeList(id);
-//		model.addAttribute("noticeList", list);
+    return "Blog/blogNoticeBoard";
+  }
 
-		return "Blog/blogNoticeBoard";
+  // 공지사항 하나 가져오기.
+  @RequestMapping(value = "blog_noticeDetail/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_noticeDetail(@PathVariable("id") String id, @RequestParam("no") int no,
+      @RequestParam(value = "keyword", required = false) String keyword, Model model) {
+    System.out.println("[TestingController.blog_notice()]");
 
-	}
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute("blogVo", blogVo);
 
-	@RequestMapping(value = "blog_noticeDetail/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_noticeDetail(@PathVariable("id") String id, Model model) {
-		System.out.println("[TestingController.blog_notice()]");
+    // 공지사항 정보
+    NoticeVo noticeVo = blogService.getNotice(no);
+    model.addAttribute("noticeVo", noticeVo);
+    System.out.println(noticeVo);
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    // 서치 값 맵에 담기(리스트)
+    Map<String, Object> searchvalue = new HashMap<String, Object>();
+    searchvalue.put("search", keyword);
+    searchvalue.put("id", id);
 
+    // 리스트 가져오기
+    List<NoticeVo> noticeList = blogService.noticeList(searchvalue);
+    System.out.println(noticeList);
+    model.addAttribute("noticeList", noticeList);
 
-		return "Blog/blogNoticeDetail";
+    return "Blog/blogNoticeDetail";
+  }
 
-	}
+  @RequestMapping(value = "blog_write/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_writeform(@PathVariable("id") String id, Model model) {
+    System.out.println("[TestingController.blog_writeForm()]");
 
-	@RequestMapping(value = "blog_write/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_writeform(@PathVariable("id") String id, Model model) {
-		System.out.println("[TestingController.blog_writeForm()]");
+    // 해더 정보 가져오기
+    BlogVo blogVo = blogService.selectUser(id);
+    model.addAttribute(blogVo);
 
-		// 해더 정보 가져오기
-		BlogVo blogVo = blogService.selectUser(id);
-		model.addAttribute(blogVo);
+    return "Blog/blogWriteForm";
+  }
 
-		return "Blog/blogWriteForm";
+  @RequestMapping(value = "writePost/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String blog_writeform(@PathVariable("id") String id, @ModelAttribute NoticeVo noticeVo) {
+    System.out.println("[TestingController.writePost()]");
+    System.out.println("들어온 정보: " + noticeVo);
 
-	}
+    blogService.writePost(noticeVo);
 
-	@RequestMapping(value = "writePost/{id}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String blog_writeform(@PathVariable("id") String id,
-								 @ModelAttribute NoticeVo noticeVo) {
-		System.out.println("[TestingController.writePost()]");
-		System.out.println("들어온 정보: " + noticeVo);
-
-		blogService.writePost(noticeVo);
-
-		return "redirect:/blog/blog_main/" + id;
-
-	}
+    return "redirect:/blog/blog_main/" + id;
+  }
 
 }
--- src/main/java/com/javaex/service/BlogService.java
+++ src/main/java/com/javaex/service/BlogService.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,90 +18,101 @@ import com.javaex.vo.ResumeVo;
 
 @Service
 public class BlogService {
-    
-    @Autowired
-    BlogDao blogDao;
-    
-    //블로그 정보 가져오기
-    public BlogVo selectUser (String id) {
-        System.out.println("BlogService.SelectUser()");
-        
-        return blogDao.selectUser(id);
-    }
-    
-    //블로그 이력사항 가져오기
-    public List<ResumeVo> getResumeList(String id){
-        System.out.println("BlogService.getResumeList()");
-        
-        return blogDao.selectResumeList(id);
-    }
-    
-    //블로그 수정하기(이미지)
-    public int modifyImg(BlogVo blogVo, MultipartFile file, List<ResumeVo> resumeList) {
-        System.out.println("BlogService.Modify()");
-        
-        String saveDir = "C:\\javaStudy\\upload";
 
-        // 확장자
-        String exName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-        System.out.println("exName:" + exName);
-        
-        // 저장파일이름(관리 떄문에 겹치지 않는 새이름 부여)
-        String logoFile = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
-        System.out.println("saveName:" + logoFile);
+  @Autowired
+  BlogDao blogDao;
 
-        // 파일패스
-        String filePath = saveDir + "\\" + logoFile;
-        System.out.println("filePath:" + filePath);
-        
-        // 파일 서버하드디스크에 저장
-        try {
+  // 블로그 정보 가져오기
+  public BlogVo selectUser(String id) {
+    System.out.println("BlogService.SelectUser()");
 
-            byte[] fileData = file.getBytes();
-            OutputStream out = new FileOutputStream(filePath);
-            BufferedOutputStream bout = new BufferedOutputStream(out);
+    return blogDao.selectUser(id);
+  }
 
-            bout.write(fileData);
-            bout.close();
+  // 블로그 이력사항 가져오기
+  public List<ResumeVo> getResumeList(String id) {
+    System.out.println("BlogService.getResumeList()");
 
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        
-        blogVo.setBanner(logoFile);
-        blogDao.resumeInsert(resumeList, blogVo);
-        
-        return blogDao.modifyImg(blogVo);   
+    return blogDao.selectResumeList(id);
+  }
+
+  // 블로그 수정하기(이미지)
+  public int modifyImg(BlogVo blogVo, MultipartFile file, List<ResumeVo> resumeList) {
+    System.out.println("BlogService.Modify()");
+
+    String saveDir = "C:\\javaStudy\\upload";
+
+    // 확장자
+    String exName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+    System.out.println("exName:" + exName);
+
+    // 저장파일이름(관리 떄문에 겹치지 않는 새이름 부여)
+    String logoFile = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
+    System.out.println("saveName:" + logoFile);
+
+    // 파일패스
+    String filePath = saveDir + "\\" + logoFile;
+    System.out.println("filePath:" + filePath);
+
+    // 파일 서버하드디스크에 저장
+    try {
+
+      byte[] fileData = file.getBytes();
+      OutputStream out = new FileOutputStream(filePath);
+      BufferedOutputStream bout = new BufferedOutputStream(out);
+
+      bout.write(fileData);
+      bout.close();
+
+    } catch (Exception e) {
+      // TODO: handle exception
     }
-    
-    //블로그 수정하기(노 이미지)
-    public int modify(BlogVo blogVo, List<ResumeVo> resumeList) {
-        System.out.println("BlogService.Modify()");
-        
-        blogDao.resumeInsert(resumeList, blogVo);
-        return blogDao.modify(blogVo);  
-    }
-    
-    //블로그 수정하기(이미지 삭제)
-    public int modifyDeleteImg(BlogVo blogVo, List<ResumeVo> resumeList) {
-        System.out.println("BlogService.Modify()");
-        
-        blogDao.resumeInsert(resumeList, blogVo);
-        return blogDao.modifyDeleteImg(blogVo); 
-    }
-    
-    // Write Post (글 작성)
-    public int writePost(NoticeVo noticeVo) {
-        System.out.println("BlogService.writePost()");
-        
-        return blogDao.writePost(noticeVo);
-    }
-    
-    //Notice List (공지사항 리스트 가져오기)
-    public List<NoticeVo> noticeList(String id) {
-        System.out.println("BlogService.noticeList()");
-        
-        return blogDao.noticeList(id);
-    }
+
+    blogVo.setBanner(logoFile);
+    blogDao.resumeInsert(resumeList, blogVo);
+
+    return blogDao.modifyImg(blogVo);
+  }
+
+  // 블로그 수정하기(노 이미지)
+  public int modify(BlogVo blogVo, List<ResumeVo> resumeList) {
+    System.out.println("BlogService.Modify()");
+
+    blogDao.resumeInsert(resumeList, blogVo);
+    return blogDao.modify(blogVo);
+  }
+
+  // 블로그 수정하기(이미지 삭제)
+  public int modifyDeleteImg(BlogVo blogVo, List<ResumeVo> resumeList) {
+    System.out.println("BlogService.Modify()");
+
+    blogDao.resumeInsert(resumeList, blogVo);
+    return blogDao.modifyDeleteImg(blogVo);
+  }
+
+  // Write Post (글 작성)
+  public int writePost(NoticeVo noticeVo) {
+    System.out.println("BlogService.writePost()");
+
+    return blogDao.writePost(noticeVo);
+  }
+
+  // Notice List (공지사항 리스트 가져오기)
+  public List<NoticeVo> noticeList(Map<String, Object> searchvalue) {
+    System.out.println("BlogService.noticeList()");
+
+    return blogDao.noticeList(searchvalue);
+  }
+
+  // 공지사항 1개 가져오기
+  public NoticeVo getNotice(int no) {
+    System.out.println("BlogService.getBoard()");
+
+    // Hit + 1
+    blogDao.updateHit(no);
+
+    // Get Board DB
+    return blogDao.getNotice(no);
+  }
 
 }
